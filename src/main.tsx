79ac434b08d3fb4276ee0078573984746ad1a93d
Joel Samuel <joel.sr1024@gmail.com>
2023-09-07 13:28:52 +0530

    added icons

--- src/main.tsx
+++ src/main.tsx
@@ -5,13 +5,15 @@ import { LiveError, LivePreview, LiveProvider } from 'react-live';
 import * as grafanaRuntime from '@grafana/runtime';
 import { tw } from 'twind';
 import axios from "axios"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import * as icons from '@fortawesome/free-solid-svg-icons'
 
 interface Props extends PanelProps<PanelOptions> {}
 
 export const Panel: React.FC<Props> = ({ options: { code }, data, width, height }) => {
   return (
     <div>
-      <LiveProvider code={code} scope={{ data, width, height, React, grafanaRuntime, axios, tw }} noInline={true}>
+      <LiveProvider code={code} scope={{ data, width, height, React, grafanaRuntime, axios, tw, FontAwesomeIcon, icons }} noInline={true}>
         <LiveError />
         <LivePreview />
       </LiveProvider>
