eb866fb39a7103e5c17a06d146b8d27138fe0934
useEffects <joel.sr1024@gmail.com>
2023-12-26 20:18:46 +0530

    add grafana ui

--- src/main.tsx
+++ src/main.tsx
@@ -7,13 +7,14 @@ import { tw } from 'twind';
 import axios from "axios"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import * as icons from '@fortawesome/free-solid-svg-icons'
+import * as grafanaUi from "@grafana/ui"
 
 interface Props extends PanelProps<PanelOptions> { }
 
 export const Panel = ({ options: { code }, data, width, height }: Props) => {
   return (
     <div>
-      <LiveProvider code={code} scope={{ data, width, height, React, grafanaRuntime, axios, tw, FontAwesomeIcon, icons }} noInline={true}>
+      <LiveProvider code={code} scope={{ data, width, height, React, grafanaRuntime, axios, tw, FontAwesomeIcon, icons, grafanaUi }} noInline={true}>
         <LiveError />
         <LivePreview />
       </LiveProvider>
