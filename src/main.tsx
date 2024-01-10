dbce5298e063a54c90d3f379e48dce2f78e8411b
aprilia <joel.sr1024@gmail.com>
2024-01-10 20:43:44 +0530

    add fonts

--- src/main.tsx
+++ src/main.tsx
@@ -7,14 +7,16 @@ import { tw } from 'twind';
 import axios from "axios"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import * as icons from '@fortawesome/free-solid-svg-icons'
+import * as brandIcons from "@fortawesome/free-brands-svg-icons"
 import * as grafanaUi from "@grafana/ui"
+import "./styles.css"
 
 interface Props extends PanelProps<PanelOptions> { }
 
 export const Panel = ({ options: { code }, data, width, height }: Props) => {
   return (
     <div>
-      <LiveProvider code={code} scope={{ data, width, height, React, grafanaRuntime, axios, tw, FontAwesomeIcon, icons, grafanaUi }} noInline={true}>
+      <LiveProvider code={code} scope={{ data, width, height, React, grafanaRuntime, axios, tw, FontAwesomeIcon, icons, grafanaUi, brandIcons }} noInline={true}>
         <LiveError />
         <LivePreview />
       </LiveProvider>
